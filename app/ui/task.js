d05fed7bfac3c120e66d30c137ec8980b2544c94
Hani Kaabi <hani@hnkb.de>
2024-03-09 11:35:52 +0100

    Show tasks in the queue in reverse (newer to older) order

--- app/ui/task.js
+++ app/ui/task.js
@@ -2,10 +2,15 @@
 export function updateTaskList(taskList) {
     const list = document.querySelector('.tasks ul');
     list.innerHTML = '';
-    for (const task of taskList.tasks) {
+
+    const fragment = document.createDocumentFragment();
+    for (let i = taskList.tasks.length - 1; i >= 0; i--) {
+        const task = taskList.tasks[i];
         const item = document.createElement('li');
         item.textContent = `${task.name} (${task.row}, ${task.col})`;
         item.className = task.state;
-        list.appendChild(item);
+        fragment.appendChild(item);
     }
+
+    list.appendChild(fragment);
 }
